192864c35c7e49541b2a1c53bb8220cacc702839
Christopher Forti <christopher.forti.79@gmail.com>
2023-10-15 19:01:59 -0400

    Changed app.js back to test.js.

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-const { format } = require("date-fns");
 
 app.get("/", (req, res) => {
   res.send("<h1>Homepage</h1><a href='/info'>Info page</a>");
